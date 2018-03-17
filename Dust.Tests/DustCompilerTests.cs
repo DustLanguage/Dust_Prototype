333c576d928273576c0dcd0065ab1d808357dbc1
Nufflee <zifcani@gmail.com>
2018-03-17 20:16:37 +0100

    [Tests] Add compiler tests.

---
+++ Dust.Tests/DustCompilerTests.cs
@@ -0,0 +1,24 @@
+﻿using Antlr4.Runtime;
+using Dust.Compiler;
+using Dust.Language;
+using Dust.Language.Nodes;
+using Xunit;
+
+namespace Dust.Tests
+{
+  public static class DustCompilerTests
+  {
+    private static object Compile(string input)
+    {
+      AntlrInputStream inputStream = new AntlrInputStream(input);
+      DustLexer lexer = new DustLexer(inputStream);
+      CommonTokenStream commonTokenStream = new CommonTokenStream(lexer);
+      DustParser parser = new DustParser(commonTokenStream);
+      DustVisitor visitor = new DustVisitor(new DustContext());
+
+      DustRuntimeCompiler compiler = new DustRuntimeCompiler(new DustContext());
+
+      return compiler.CompileModule((Module) visitor.VisitModule(parser.module()));
+    }
+  }
+}
