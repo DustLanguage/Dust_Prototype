f7f63addff27e028ec96eec19487a7c702552597
PCHome <16463535+TheChoconut@users.noreply.github.com>
2018-03-17 22:31:57 +0100

    [Tests] Add more unit tests.

--- Dust.Tests/DustCompilerTests.cs
+++ Dust.Tests/DustCompilerTests.cs
@@ -20,5 +20,17 @@ namespace Dust.Tests
 
       return compiler.CompileModule((Module) visitor.VisitModule(parser.module()));
     }
+
+    [Theory]
+    [InlineData("1+2+3+4", 10)]
+    [InlineData("12345*678*9", 75329190)]
+    [InlineData("4+20+100", 124)]
+    [InlineData("7.5*7.5", 56.25f)]
+    [InlineData("4/20*100", 20f)]
+    public static void Test_ParseSimpleMultipleArthimeticOperations(string input, int expected)
+    {
+      Assert.Equal(Compile(input), expected);
+    }
+    
   }
 }
